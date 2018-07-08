b08ec69c6d88f405ebc8071a4a8f9c9146841fc3
Pierre Busnel <pierre@MacPro.local>
2018-07-08 16:40:06 -0400

    changed

--- src/main/java/com/pierre2803/downloader/DownloaderApp.java
+++ src/main/java/com/pierre2803/downloader/DownloaderApp.java
@@ -1,14 +1,19 @@
 package com.pierre2803.downloader;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.List;
+
 @SpringBootApplication
-@EnableScheduling
 public class DownloaderApp {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application");
 		SpringApplication.run(DownloaderApp.class, args);
 	}
 
--- src/main/java/com/pierre2803/downloader/service/DownloaderService.java
+++ src/main/java/com/pierre2803/downloader/service/DownloaderService.java
@@ -4,24 +4,38 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
 public class DownloaderService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DownloaderService.class);
-
-    @Value("${parser.wanderlust.baseUrl}")
-    private String baseUrl;
-
-    @Value("${parser.wanderlust.icsfilename}")
-    private String icsfilename;
 
     String httpFile = "http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso";
     String httpFile2 = "http://apache.mirror.globo.tech//httpd/httpd-2.4.33.tar.bz2";
 
+    @Value("${project.name}")
+    String projectName;
+
+    @Value("${server}")
+    String server;
+
+    @Value("${download.servers}")
+    List<String> servers = new ArrayList<String>();
+
     public int start (){
-        System.out.println("START DOWNLOAD");
+        System.out.println(projectName);
+        System.out.println("START DOWNLOAD "+server+" " + servers);
+        for(int i=0; i<servers.size(); i++)
+            System.out.println("servers: " + servers.get(i));
 
 
         File dstFile = null;
